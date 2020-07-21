d6f2f9dda80bcd13d2a224b6abe1f2f3438a0ba2
skinnystick22 <tyler.e.miller1995@gmail.com>
2020-07-21 12:41:46 -0500

    Added new item to the iOS project for TipView

---
+++ TipCalc.Ios/Views/TipView.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using UIKit;
+
+namespace TipCalc.Ios.Views
+{
+    public partial class TipView : UIViewController
+    {
+        public TipView() : base("TipView", null)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
+
