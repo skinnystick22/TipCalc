aaa1a3ec58bceabd494e09a9b000737bae413dac
Tyler Miller <tyler.e.miller1995@gmail.com>
2020-07-21 12:12:49 -0500

    Initial Solution and Project Created

---
+++ TipCalc.Core/ViewModels/TipViewModel.cs
@@ -0,0 +1,68 @@
+﻿using System.Threading.Tasks;
+using MvvmCross.ViewModels;
+using TipCalc.Core.Services;
+
+namespace TipCalc.Core.ViewModels
+{
+    public class TipViewModel : MvxViewModel
+    {
+        private readonly ICalculationService _calculationService;
+
+        private double _subTotal, _tip;
+        private int _generosity;
+
+        public double SubTotal
+        {
+            get => _subTotal;
+            set
+            {
+                _subTotal = value;
+                RaisePropertyChanged(() => SubTotal);
+
+                Recalculate();
+            }
+        }
+
+        public int Generosity
+        {
+            get => _generosity;
+            set
+            {
+                _generosity = value;
+                RaisePropertyChanged(() => Generosity);
+
+                Recalculate();
+            }
+        }
+
+        public double Tip
+        {
+            get => _tip;
+            set
+            {
+                _tip = value;
+                RaisePropertyChanged(() => Tip);
+            }
+        }
+
+        public TipViewModel(ICalculationService calculationService)
+        {
+            _calculationService = calculationService;
+        }
+
+        public override async Task Initialize()
+        {
+            await base.Initialize();
+
+            _subTotal = 100;
+            _generosity = 10;
+
+            Recalculate();
+        }
+
+        private void Recalculate()
+        {
+            Tip = _calculationService.TipAmount(SubTotal, Generosity);
+        }
+    }
+}
