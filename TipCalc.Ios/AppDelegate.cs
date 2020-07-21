e436318d71b5d0edc2be12b04a55b7ced8355ef3
Tyler Miller <tyler.e.miller1995@gmail.com>
2020-07-21 12:13:17 -0500

    iOS Project Added

---
+++ TipCalc.Ios/AppDelegate.cs
@@ -0,0 +1,23 @@
+﻿using Foundation;
+using MvvmCross.Platforms.Ios.Core;
+using TipCalc.Core;
+using UIKit;
+
+namespace TipCalc.Ios
+{
+    [Register(nameof(AppDelegate))]
+    public class AppDelegate : MvxApplicationDelegate<MvxIosSetup<App>, App>
+    {
+        public override UIWindow Window { get; set; }
+
+        // FinishedLaunching is the very first code to be executed in your app. Don't forget to call base!
+        public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
+        {
+            var result = base.FinishedLaunching(application, launchOptions);
+
+            return result;
+        }
+    }
+}
+
+
